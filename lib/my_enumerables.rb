8ee41b67e2c1d71b16ff554e05c52fb44401f28d
Hee Ran Han <heeranh93@gmail.com>
2024-05-12 01:10:21 -0700

    first commit

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,4 +8,112 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    if !block_given?
+      return to_enum(:my_each)
+    end
+
+    for elem in self
+      yield elem
+    end
+    return self
+  end
+
+  def my_each_with_index
+    if !block_given?
+      return to_enum(:my_each_with_index)
+    end
+
+    i = 0
+    while i < self.length do
+      yield self[i], i
+      i += 1
+    end
+    return self
+  end
+
+  def my_select
+    if !block_given?
+      return to_enum(:my_select)
+    end
+
+    result = []
+    for elem in self
+      if yield(elem)
+        result << elem
+      end
+    end
+    return result
+  end
+
+  def my_all?
+    if !block_given?
+      return true
+    end
+
+    for elem in self
+      if !yield(elem)
+        return false
+      end
+    end
+    return true
+  end
+
+  def my_any?
+    if !block_given?
+      return true
+    end
+
+    for elem in self
+      if yield(elem)
+        return true
+      end
+    end
+    return false
+  end
+
+  def my_none?
+    if !block_given?
+      return false
+    end
+
+    for elem in self
+      if yield(elem)
+        return false
+      end
+    end
+    return true
+  end
+
+  def my_count
+    count = self.length
+    if block_given?
+      count = 0
+      for elem in self
+        count += 1 unless !yield(elem)
+      end
+    end
+    return count
+  end
+
+  def my_map
+    if !block_given?
+      return to_enum(:my_map)
+    end
+
+    result = []
+    for elem in self
+      result << yield(elem)
+    end
+    return result
+  end
+
+  def my_inject(initial_value)
+    result = initial_value
+    for elem in self
+      result = yield(result, elem)
+    end
+    return result
+  end
+
 end
